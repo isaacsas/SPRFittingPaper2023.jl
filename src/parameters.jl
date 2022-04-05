1db42b0ece0f934770a50f3bf31d975f558687cb
Sam Isaacson <isaacsas@users.noreply.github.com>
2022-04-05 18:07:57 -0400

    fix bug

--- src/parameters.jl
+++ src/parameters.jl
@@ -56,7 +56,6 @@ function Base.show(io::IO, ::MIME"text/plain", sps::SimParams)
     println(io, "tstop_AtoB = ", tstop_AtoB)
     println(io, "save frequency (dt) = ", dt)
     println(io, "domain length (L) = ", L)
-    println(io, "dimension of domain = ", DIM)
     println(io, "resample_initlocs = ", resample_initlocs)
     print(io, "nsims = ", nsims)
 end
