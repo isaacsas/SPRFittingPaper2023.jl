31c3d6d9879f45eb8d7aedf1a6b2cfcc6cebf94e
Sam Isaacson <isaacsas@users.noreply.github.com>
2022-07-14 16:59:32 -0400

    update scripts

--- examples/cluster_scripts/queue_job.sh
+++ examples/cluster_scripts/queue_job.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 PKGPATH="/project/fpkmc3d/.julia_sai/dev/SPRFitting"
-METADATAFILE="/Users/isaacsas/data/2022-06-07 - FD11A_Data/surrogates/test_metadata.jld"
-OUTFILEBASENAME="/Users/isaacsas/data/2022-06-07 - FD11A_Data/surrogates/test_slice"
-JULIASCRIPT="make_surrogate_parallel.jl"
+METADATAFILE="/project/fpkmc3d/surrogates/tests/test_metadata.jld"
+OUTFILEBASENAME="/project/fpkmc3d/surrogates/tests/test_slice"
+JULIASCRIPT="${PKGPATH}/examples/make_surrogate_parallel.jl"
 NUMPARAMSETS=8
 NCPUS=4
 
@@ -12,6 +12,6 @@ let dn=$IDXINCREMENT-1
 let idx=1
 for (( n=1;n<=$NUMPARAMSETS;n+=$IDXINCREMENT)); do
     let last=$n+$dn
-    qsub run_sim.sh "$JULIASCRIPT" "$PKGPATH" "$METADATAFILE" "${OUTFILEBASENAME}_${idx}.jld" $n $last
+    echo qsub run_sim.sh "$JULIASCRIPT" "$PKGPATH" "$METADATAFILE" "${OUTFILEBASENAME}_${idx}.jld" $n $last
     let idx+=1
 done
