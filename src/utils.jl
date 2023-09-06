c78725802aec5e89a3249263e910bd6291cf9bd6
Sam Isaacson <isaacsas@users.noreply.github.com>
2023-09-05 20:20:17 -0400

    fix test

--- src/utils.jl
+++ src/utils.jl
@@ -1,11 +1,11 @@
-# unit conversions 
+# unit conversions
 
 # # transforms RU to μM (is this really always right?)
-# RU_to_muM(x) = x/149/26795*1000000 
+# RU_to_muM(x) = x/149/26795*1000000
 
 # # transforms RU to (nm)⁻³
 # RU_to_inv_cubic_nm(x) = x * (.6023/149/26795)
 
 # transforms μM to (nm)⁻³ and vice versa
-muM_to_inv_cubic_nm(x) = 6.023e-7 * x
-inv_cubic_nm_to_muM(x) = x / 6.023e-7
+muM_to_inv_cubic_nm(x) = 6.02214076e-7 * x
+inv_cubic_nm_to_muM(x) = x / 6.02214076e-7
